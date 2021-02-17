6d3a1461120f250f4f9fdcf072548227ea4f850c
Akshat Sethi <masterakshata@gmail.com>
2021-02-18 03:24:14 +0530

    implement login,sign_up,sign_in and sessions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def flash_shower
+    unless flash[:error].nil? && flash[:notice].nil? && flash[:alert].nil? 
+      '<div class="flash">' \
+          "#{flash[:error] || flash[:notice] || flash[:alert]}" \
+      "</div>" 
+    end 
+  end
+
+  def signed_in?
+    !params[:user_id].nil?
+  end
+
+  def current_user
+    if signed_in?
+      User.find(params[:user_id])
+    else
+      'Log in'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :sessions
+
+  post 'login' => 'sessions#create'
+  get 'sign_in' => 'sessions#new'
+  get 'sign_out' => 'sessions#destroy'
+  get 'sign_up' => 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
