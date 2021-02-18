e47b3627e901ae827f6b1d129009413737eff230
Akshat Sethi <masterakshata@gmail.com>
2021-02-18 15:30:33 +0530

    add before_action authenticat_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+
+   
+    def authenticate_user!
+        unless helpers.signed_in?
+            redirect_to sign_in_path
+        end
+    end 
 end
