8ca2c2b15424a245e3d6a0b64aed666be1a87bd0
Akshat Sethi <masterakshata@gmail.com>
2021-02-25 21:54:38 +0530

    fix linter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,9 @@
 module ApplicationHelper
   def flash_shower
-    unless flash[:error].nil? && flash[:notice].nil? && flash[:alert].nil?
+    return if flash[:error].nil? && flash[:notice].nil? && flash[:alert].nil?
       '<div class="flash">' \
           "#{flash[:error] || flash[:notice] || flash[:alert]}" \
       '</div>'
-    end
   end
 
   def signed_in?
